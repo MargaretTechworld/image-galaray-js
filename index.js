46d3ea18528379d829e2b7a2fca4e3bbf24009da
MargaretTechworld <margarettechworld@gmail.com>
2024-01-31 00:35:09 +0000

    Add linters coonfigurations

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let scrollContainer = document.querySelector(".gallery");
-let backBtn = document.getElementById("backBtn");
-let nextBtn = document.getElementById("nextBtn");
+const scrollContainer = document.querySelector('.gallery');
+const backBtn = document.getElementById('backBtn');
+const nextBtn = document.getElementById('nextBtn');
 
-backBtn.addEventListener("click", () => {
-    scrollContainer.scrollBy({
-        left: -scrollContainer.clientWidth,
-        behavior: "smooth"
-    });
+backBtn.addEventListener('click', () => {
+  scrollContainer.scrollBy({
+    left: -scrollContainer.clientWidth,
+    behavior: 'smooth',
+  });
 });
 
-nextBtn.addEventListener("click", () => {
-    scrollContainer.scrollBy({
-        left: scrollContainer.clientWidth,
-        behavior: "smooth"
-    });
+nextBtn.addEventListener('click', () => {
+  scrollContainer.scrollBy({
+    left: scrollContainer.clientWidth,
+    behavior: 'smooth',
+  });
 });
 
-scrollContainer.addEventListener("wheel", (event) => {
-    event.preventDefault();
+scrollContainer.addEventListener('wheel', (event) => {
+  event.preventDefault();
 
-    scrollContainer.scrollBy({
-        left: event.deltaY > 0 ? 100 : -100,
-        behavior: "smooth"
-    });
+  scrollContainer.scrollBy({
+    left: event.deltaY > 0 ? 100 : -100,
+    behavior: 'smooth',
+  });
 });
